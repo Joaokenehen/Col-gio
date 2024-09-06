9a412350e171d7722276491e9b3d2796744f4a04
Joaokenehen <115033870+Joaokenehen@users.noreply.github.com>
2024-09-06 16:48:31 -0300

    Update Mensagem Main.java

--- MainEstudos/src/Main.java
+++ MainEstudos/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,41 +6,49 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+
         Scanner scanner = new Scanner(System.in);
         Meses meses = new Meses();
 
         Mensagem msg = new Mensagem();
-
-        System.out.println("Digite o nome de um pai");
-        msg.nomePai = scanner.nextLine();
-
-        System.out.println("Digite o nome do filho");
-        msg.nomeFilho = scanner.nextLine();
-
-        System.out.println("(1 = Masculino) (2 = Feminino)");
-        int sex = scanner.nextInt();
-
-        System.out.println("Digite o primeiro mês até o ultimo que está em aberto");
-        meses.rec1 = scanner.nextInt();
-        meses.rec2 = scanner.nextInt();
+        char escolha;
+        do {
 
 
-        if (sex == 2 && meses.rec1 == meses.rec2) {
-            msg.feminino();
-            meses.mes(meses.rec1, meses.rec2);
-            System.out.print("ainda está em aberto.");
-        } else if (sex == 2 && meses.rec1 != meses.rec2) {
-            msg.feminimoMais();
-            meses.mes(meses.rec1, meses.rec2);
-            System.out.print("ainda estão em aberto.");
-        } else if (sex == 1 && meses.rec1 == meses.rec2) {
-            msg.masculino();
-            meses.mes(meses.rec1, meses.rec2);
-            System.out.print("ainda está em aberto.");
-        } else if (sex == 1 && meses.rec1 != meses.rec2) {
-            msg.masculinoMais();
-            meses.mes(meses.rec1, meses.rec2);
-            System.out.print("ainda estão em aberto.");
-        }
+            System.out.println("Digite o nome de um pai");
+            msg.nomePai = scanner.next();
+
+            System.out.println("Digite o nome do filho");
+            msg.nomeFilho = scanner.next();
+
+            System.out.println("(1 = Masculino) (2 = Feminino)");
+            int sex = scanner.nextInt();
+
+            System.out.println("Digite o primeiro mês até o ultimo que está em aberto");
+            meses.rec1 = scanner.nextInt();
+            meses.rec2 = scanner.nextInt();
+
+
+            if (sex == 2 && meses.rec1 == meses.rec2) {
+                msg.feminino();
+                meses.mes(meses.rec1, meses.rec2);
+                System.out.print("ainda está em aberto.\n");
+            } else if (sex == 2 && meses.rec1 != meses.rec2) {
+                msg.feminimoMais();
+                meses.mes(meses.rec1, meses.rec2);
+                System.out.print("ainda estão em aberto.\n");
+            } else if (sex == 1 && meses.rec1 == meses.rec2) {
+                msg.masculino();
+                meses.mes(meses.rec1, meses.rec2);
+                System.out.print("ainda está em aberto.\n");
+            } else if (sex == 1 && meses.rec1 != meses.rec2) {
+                msg.masculinoMais();
+                meses.mes(meses.rec1, meses.rec2);
+                System.out.print("ainda estão em aberto.\n");
+            }
+
+            System.out.println("Quer continuar a aplicação? (s = Continuar) n = Fechar)");
+             escolha = scanner.next().charAt(0);
+        }while (escolha != 2);
     }
 }
